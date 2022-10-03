ab3f06630d4edb294a6ba07983cd47dc46e362d5
kilian beaumont <kilian.beaumont@gmail.com>
2022-10-03 11:54:16 +0200

    commentaires + users + diagrammes de classe

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ArticleController extends AbstractController
 {
@@ -18,19 +25,23 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/articles', name: 'app_articles')]
-
     // A l'appel de la méthode, SYMFONY va
     // créer un objet de la classe repository et le passer
     // en paramètre de la méthode
     // Mécanisme : l'injection de dépendances
-
-    public function getArticles(): Response
+    public function getArticles(PaginatorInterface $paginator, Request $request): Response
     {
         // Récupérer les informations dans la bdd
         // Le controller fait appel au modèle (une classe du modèle)
         // afin de récupérer la liste des articles
         // $repository = new ArticleRepository();
-        $articles = $this->articleRepository->findBy([],['createdat'=>'DESC']);
+
+        // Mise en place de la pagination
+        $articles = $paginator->paginate(
+            $this->articleRepository->findBy([],['createdat'=>'DESC']), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
 
 
         return $this->render('article/index.html.twig',[
@@ -39,8 +50,7 @@ class ArticleController extends AbstractController
 
     }
 
-    #[Route('/article/{slug}', name: 'app_article_slug')]
-
+    #[Route('/articles/{slug}', name: 'app_article_slug')]
     public function getArticle($slug): Response
     {
 
@@ -52,6 +62,28 @@ class ArticleController extends AbstractController
 
         ]);
 
+    }
+
+    #[Route('/articles/nouveau', name: 'app_articles_nouveau',priority: 1)]
+    public function insert(SluggerInterface $slugger) : Response {
+        $article = new Article();
+        // Création du formulaire
+        $formArticle = $this->createForm(ArticleType::class, $article);
+        // Appel de la vue twig permettant d'afficher le formulaire
+        return $this->renderForm('articles/nouveau.html.twig', [
+            "formArticle"=>$formArticle
+        ]);
+
+
+        /*$article->setTitre('Nouvel article 2')
+            ->setContenu("Contenu du nouvel article 2")
+            ->setSlug($slugger->slug($article->getTitre())->lower())
+            ->setCreatedat(new \DateTime());
+
+        // Only avec Symfony 6 !
+        $this->articleRepository->add($article, true);
+
+        return $this->redirectToRoute("app_articles");*/
     }
 
 
